30850f02c0ed5e4eeb459808d5b10dcb5024cfb1
jamie <xinjun@peachpay.app>
2024-03-27 12:22:28 -0500

    remove arrow img and improve scrolling behaviour

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ if (button) {
         experienceDark ? experienceDark.classList.toggle('hide') : '';
     });
 }
+
+// For scrolling to different sections of a page
+const scrollToProfile = document.querySelector('.my-logo');
+const profileSection = document.querySelector('section#profile');
+if (scrollToProfile && profileSection) {
+    scrollToProfile.addEventListener('click', () => {
+        profileSection.scrollIntoView({behavior: "smooth"});
+    });
+}
+
+const scrollToAbout = document.querySelector('#scroll-about');
+const aboutSection = document.querySelector('section#about');
+if (scrollToAbout && aboutSection) {
+    scrollToAbout.addEventListener('click', () => {
+        aboutSection.scrollIntoView({behavior: "smooth"});
+    });
+}
+
+const scrollToContact = document.querySelector('#scroll-contact');
+const contactSection = document.querySelector('section#contact');
+if (scrollToContact && contactSection) {
+    scrollToContact.addEventListener('click', () => {
+        contactSection.scrollIntoView({behavior: "smooth"});
+    });
+}
