d90a04b645867a386acb1252673831b468aad2ee
jamie <xinjun@peachpay.app>
2024-10-24 23:05:08 +0800

    removed unused about html and redo contact section

--- script.js
+++ script.js
@@ -95,3 +95,46 @@ resumeMobile.addEventListener('click', () => {
         resumes.classList.toggle('hide');
     }
 });
+
+// For handling the submission of email web3form, according to https://web3forms.com/platforms/javascript-contact-form
+const emailForm = document.getElementById('email-form');
+const result = document.getElementById('result');
+if (emailForm) {
+    emailForm.addEventListener('submit', event => {
+        const formData = new FormData(emailForm);
+        event.preventDefault();
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        result.innerHTML = "Please wait..."
+
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            emailForm.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+    })
+}
