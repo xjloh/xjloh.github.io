4d463f55f7c0a5a312e8c77090a9b47f8be40b98
jamie <xinjun@peachpay.app>
2025-06-08 01:34:05 +0800

    added about section and initial code for experience section

---
+++ app/experiences/experience.tsx
@@ -0,0 +1,174 @@
+const Experiences = () => {
+  return (
+    <>
+      {/* <div className="section-container column">
+        <div className="text-container">
+          <h1 className="title">My experience</h1>
+        </div>
+        <div className="my-experience-containers">
+          <div className="experience-container floating-effect">
+            <div className="experience-info">
+              <div className="experience-title">
+                <div>
+                  <h2>
+                    <b>PeachPay</b>
+                  </h2>
+                </div>
+                <div>
+                  <p>Full-stack engineer (2021-2024)</p>
+                </div>
+              </div>
+              <div className="expand-indicator">&gt;</div>
+            </div>
+            <div className="experience-details hide">
+              <br />
+              <p>
+                <b>Skills:</b> Typescript, Javascript, PHP, HTML, SCSS, CSS
+              </p>
+              <p>
+                <b>Testing suite:</b> Playwright
+              </p>
+              <br />
+              <h3>Brief introduction:</h3>
+              <p>
+                PeachPay is a software company founded in 2020 that specializes
+                in streamlining the checkout process for customers with minimal
+                technical issues, thus improving sales volumes and reducing cart
+                abandonment. Additionally, PeachPay offers multiple different
+                features such as customizable checkout window, currency
+                switcher, payment methos (Stripe, Square, PayPal, etc) and many
+                more. With all that features, PeachPay aims to improve the
+                online e-commerce experience for both customers and merchants.
+              </p>
+              <br />
+              <h3>Achievements:</h3>
+              <ul>
+                <li>
+                  Played a core role in developing early stages of the PeachPay
+                  plugin that raised $2 million dollars in venture capital,
+                  details{" "}
+                  <a
+                    //   style="text-decoration: underline;"
+                    href="https://www.crunchbase.com/organization/peachpay"
+                    target="_blank"
+                  >
+                    here
+                  </a>
+                  .
+                </li>
+                <li>
+                  Increased sales volume from $100 thousand to $1 million per
+                  month in one year through the implementation of new features
+                  and improvements to the PeachPay plugin.
+                </li>
+              </ul>
+              <br />
+              <p>
+                Click{" "}
+                <a
+                  // style="text-decoration: underline;"
+                  href="https://peachpay.app/"
+                  target="_blank"
+                >
+                  here
+                </a>{" "}
+                for more info about PeachPay
+              </p>
+            </div>
+          </div>
+          <div className="experience-container floating-effect">
+            <div className="experience-info">
+              <div className="experience-title">
+                <div>
+                  <h2>
+                    <b>InTouch</b>
+                  </h2>
+                </div>
+                <div>
+                  <p>Frontend developer (2019)</p>
+                </div>
+              </div>
+              <div className="expand-indicator">&gt;</div>
+            </div>
+            <div className="experience-details hide">
+              <br />
+              <p>
+                <b>IDE:</b> Android studio
+              </p>
+              <p>
+                <b>Skills:</b> Java, HTML, CSS, Javascript
+              </p>
+              <p>
+                <b>Testing suite:</b> JUnit
+              </p>
+              <br />
+              <h3>Summary:</h3>
+              <p>
+                InTouch is an application that connects people nearby, allowing
+                users to feel more connected to the outside world. The
+                application utilizes Google Maps API and lets users create posts
+                within their vicinity. Users can also message others within a
+                set range. This application was created for a semester-long
+                project at Iowa State University.
+              </p>
+              <br />
+              <h3>My contributions:</h3>
+              <ul>
+                <li>
+                  Led the design of the InTouch application, incorporating
+                  user-centered design principles and implementing a clean,
+                  intuitive UI for the login page, user profile page, and
+                  navigation.
+                </li>
+              </ul>
+              <br />
+              <p>
+                <p>
+                  Click{" "}
+                  <a
+                    //   style="text-decoration: underline;"
+                    href="https://github.com/xjloh/coms309"
+                    target="_blank"
+                  >
+                    here
+                  </a>{" "}
+                  for git repo
+                </p>
+              </p>
+            </div>
+          </div>
+          <div className="experience-container floating-effect">
+            <div className="experience-info">
+              <div className="experience-title">
+                <div>
+                  <h2>
+                    <b>ECG Anomaly Detection</b>
+                  </h2>
+                </div>
+                <div>
+                  <p>Frontend developer (2021)</p>
+                </div>
+              </div>
+              <div className="expand-indicator">&gt;</div>
+            </div>
+            <div className="experience-details hide">
+              <br />
+              <p>
+                <b>Skills:</b> Angular, Javascript
+              </p>
+              <br />
+              <h3>Brief summary:</h3>
+              <p>
+                Developed a webpage using Angular framework to display heartrate
+                info
+              </p>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default Experiences;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Navbar from "@/components/Navbar";
 import Profile from "./profile/page";
 import Contact from "./contact/page";
+import About from "./about/about";
+import Experiences from "./experiences/experience";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
       <Profile></Profile>
+      <About></About>
+      <Experiences></Experiences>
       <Contact></Contact>
     </>
   );
